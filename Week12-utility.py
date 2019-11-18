abd51c51ee6f6a88b9c666896c7b2f57200899f7
Brett Shearer <bpshearer@mymail.mines.edu>
2019-11-18 13:54:41 -0700

    LoadFile Added

--- Week12-utility.py
+++ Week12-utility.py
@@ -1,2 +1,12 @@
 def PrintOutput(myString):
     return "OUTPUT " + myString
+
+def LoadFile(filename):
+    file = open(filename, "r")
+    lines = file.readlines()
+    
+    for i in range(len(lines)):
+        lines[i] = lines[i].replace("\n", "")
+
+    file.close()
+    return lines
